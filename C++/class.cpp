a3a9896a766b5e63b125c0810db688ffe06ff51d
Mao <95645447+Mao0205@users.noreply.github.com>
2023-12-14 16:51:03 +0800

    Create class.cpp

---
+++ C++/class.cpp
@@ -0,0 +1,26 @@
+class Recode{
+    public:
+    int count;
+    vector<int> relation;
+    Recode():count(0){};
+    //Recode():count(0),relation({1,2,3}){};
+    //Recode():count(0),relation({}){};
+}
+
+class Recode1 {
+public:
+    int count;
+    vector<int> relation;
+
+    // 建構子
+    Recode() : count(0) {
+        int size;
+        cin >> size;
+        relation.resize(size);
+        for (int i = 0; i < size; ++i) {
+            cin >> relation[i];
+        }
+        // 設定 count 為 relation 的大小
+        count = size;
+    }
+};
